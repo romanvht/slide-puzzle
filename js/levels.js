698d7763df3d0f47b6f69ebfe49acf499b949e9d
romanvht <romanvht@gmail.com>
2024-03-20 23:31:41 +1000

    replace img and edit levels

--- js/levels.js
+++ js/levels.js
@@ -1,100 +1,19 @@
 var levels = [];
+var numLevels = 16;
+var levelSize = 3;
+var sizeUp = 4;
 
-levels[1] = {
-  "id": 1,
-  "size": 3,
-  "image": "img/1.jpg"
-};
+for (let level = 1; level <= numLevels; level++) {
+  levels[level] = {
+    "id": level,
+    "size": levelSize,
+    "image": "img/" + level + ".jpg"
+  };
 
-levels[2] = {
-  "id": 2,
-  "size": 3,
-  "image": "img/2.jpg"
-};
-
-levels[3] = {
-  "id": 3,
-  "size": 3,
-  "image": "img/14.jpg"
-};
-
-levels[4] = {
-  "id": 4,
-  "size": 3,
-  "image": "img/4.jpg"
-};
-
-levels[5] = {
-  "id": 5,
-  "size": 4,
-  "image": "img/15.jpg"
-};
-
-levels[6] = {
-  "id": 6,
-  "size": 4,
-  "image": "img/3.jpg"
-};
-
-levels[7] = {
-  "id": 7,
-  "size": 4,
-  "image": "img/7.jpg"
-};
-
-levels[8] = {
-  "id": 8,
-  "size": 4,
-  "image": "img/8.jpg"
-};
-
-levels[9] = {
-  "id": 9,
-  "size": 5,
-  "image": "img/9.jpg"
-};
-
-levels[10] = {
-  "id": 10,
-  "size": 5,
-  "image": "img/16.jpg"
-};
-
-levels[11] = {
-  "id": 11,
-  "size": 5,
-  "image": "img/11.jpg"
-};
-
-levels[12] = {
-  "id": 12,
-  "size": 5,
-  "image": "img/12.jpg"
-};
-
-levels[13] = {
-  "id": 13,
-  "size": 6,
-  "image": "img/13.jpg"
-};
-
-levels[14] = {
-  "id": 14,
-  "size": 6,
-  "image": "img/6.jpg"
-};
-
-levels[15] = {
-  "id": 15,
-  "size": 6,
-  "image": "img/5.jpg"
-};
-
-levels[16] = {
-  "id": 16,
-  "size": 6,
-  "image": "img/10.jpg"
-};
+  if(level % sizeUp === 0){
+    levelSize++;
+  }
+}
 
 function $_GET(key) {
   var s = window.location.search;
